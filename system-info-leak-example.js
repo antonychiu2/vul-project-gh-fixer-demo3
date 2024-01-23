10763c21de0651a4b30abfd411d383809d56a696
Antony Chiu <antony@mobb.ai>
2024-01-23 11:49:04 -0700

    Update system-info-leak-example.js

--- system-info-leak-example.js
+++ system-info-leak-example.js
@@ -1,5 +1,6 @@
 const http = require('node:http');
 const fs = require('node:fs/promises');
+const winston = require('winston');
 
 http.createServer((req, res) => {
     fs.readFile("./no-file.txt").catch((e) => {
